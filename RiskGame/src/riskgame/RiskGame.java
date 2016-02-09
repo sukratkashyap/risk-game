29fbc090d1b337a0ad9ffb763a7600adcdc6009b
Sukrat Kashyap <sukraat021195@gmail.com>
2016-02-09 20:01:08 +0000

    Created the netbeans project. Updated the gitignore file. Created basic template for the project.

---
+++ RiskGame/src/riskgame/RiskGame.java
@@ -0,0 +1,10 @@
+package riskgame;
+
+/**
+ * @author MiFans (Sukrat Kashyap - 14200092, Zhesi Ning - 12252511)
+ * @Description 
+ */
+public class RiskGame {
+    public static void main(String[] args) {
+    }
+}
