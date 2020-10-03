5603ba1886dc4755fcaebe8956da71c1c6748569
Sukrat Kashyap <sukraat021195@gmail.com>
2020-10-03 17:03:00 +0100

    Removing old project code

--- RiskGame/src/game/core/IValidationInput.java
+++
@@ -1,9 +0,0 @@
-package game.core;
-
-/**
- * @author MiFans (Sukrat Kashyap - 14200092, Zhesi Ning - 12252511)
- * @Description interface for validating the inputs
- */
-public interface IValidationInput {
-    Result isValid(String input);
-}
