15e15cfdfd40b9bcba68a936a043cd64c94f3ebb
Sukrat Kashyap <sukraat021195@gmail.com>
2020-10-03 17:01:59 +0100

    Moving the bot code to gradle project

---
+++ riskgamebot/src/main/java/com/sukratkashyap/riskgamebot/Bot.java
@@ -0,0 +1,14 @@
+package com.sukratkashyap.riskgamebot;
+
+public interface Bot {
+
+		public String getName ();
+		public String getReinforcement ();
+		public String getPlacement (int forPlayer);
+		public String getCardExchange ();
+		public String getBattle ();
+		public String getDefence (int countryId);
+		public String getMoveIn (int attackCountryId);
+		public String getFortify ();
+
+}
